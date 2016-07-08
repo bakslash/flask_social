9f03bc2d52d25b38d64826da42586462fd6ea250
bakslash <marcusmuasa@gmail.com>
2016-07-08 20:13:21 +0300

    hashing, class method

--- models.py
+++ models.py
@@ -1,10 +1,12 @@
 import datetime
 
+from flask.ext.bcrypt import generate_password_hash
+from flask.ext.login import UserMixin
 from peewee import *
 
 DATABASE = SqliteDatabase('social.db')
 
-class User(Model):
+class User(UserMixin, Model):
 	username = CharField(unique=True)
 	email = CharField(unique=True)
 	password = CharField(max_length=100)
@@ -14,3 +16,14 @@ class User(Model):
 	class meta:
 		database = DATABASE
 		order_by = ('-joined_at',)
+
+        @classmethod
+		def create_user(cls, username, email, password, admin=False):
+			try:
+				cls.create(
+					username=username,
+					email=email,
+					password=generate_password_hash(password),
+					is_admin=admin)
+			except IntergrityError:
+				raise ValueError("User already exist")
