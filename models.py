4c4ff9b4c55e6de743ba597a07a7db6dee2acb67
bakslash <marcusmuasa@gmail.com>
2016-07-19 13:55:27 +0300

    debug

--- models.py
+++ models.py
@@ -10,22 +10,23 @@ class User(UserMixin, Model):
 	username = CharField(unique=True)
 	email = CharField(unique=True)
 	password = CharField(max_length=100)
-	joined_at = DateTimeField(default=datetime.now)
-	is_admin = BooleaanField(default=False)
+	joined_at = DateTimeField(default=datetime.datetime.now)
+	is_admin = BooleanField(default=False)
 
 	class meta:
 		database = DATABASE
 		order_by = ('-joined_at',)
 
-        @classmethod
-		def create_user(cls, username, email, password, admin=False):
-			try:
-				cls.create(
-					username=username,
-					email=email,
-					password=generate_password_hash(password),
-					is_admin=admin)
-			except IntergrityError:
+	@classmethod
+	def create_user(cls, username, email, password, admin=False):
+		try:
+			cls.create(
+				username=username,
+				email=email,
+				password=generate_password_hash(password),
+				is_admin=admin
+				)
+		except IntergrityError:
 				raise ValueError("User already exist")
 
 
